97117b110e4207ac489277ec5dcf7993993212ef
highlander <highlander@keepkey.com>
2025-01-24 17:45:06 -0600

    dashboard work

--- apps/portfolio/src/components/keepkey/Receive.tsx
+++ apps/portfolio/src/components/keepkey/Receive.tsx
@@ -1,4 +1,4 @@
-import {Flex, Text} from "@chakra-ui/react";
+import {Flex, Text, Card, Image} from "@chakra-ui/react";
 // import QRCode from "qrcode.react";
 import React, { useEffect, useState } from 'react';
 import {Avatar} from "@/components/ui/avatar";
@@ -11,13 +11,14 @@ export function Receive({ usePioneer }: any) {
     // const { hasCopied, onCopy } = useClipboard(selectedAddress);
 
     useEffect(() => {
-        // if (assetContext && COIN_MAP_LONG[assetContext.chain as keyof typeof COIN_MAP_LONG]) {
-        //     setAvatarUrl(`https://pioneers.dev/coins/${COIN_MAP_LONG[assetContext.chain as keyof typeof COIN_MAP_LONG]}.png`);
-        // }
+        if (app.assetContext) {
+            console.log(app.assetContext);
+            setAvatarUrl(app.assetContext.icon)
+        }
         // if (assetContext?.pubkeys?.length > 0) {
         //     setSelectedAddress(assetContext.pubkeys[0].address || assetContext.pubkeys[0].master);
         // }
-    }, [assetContext]);
+    }, [app, app.assetContext]);
 
     useEffect(() => {
         if (app.pubkeys) {
@@ -33,18 +34,24 @@ export function Receive({ usePioneer }: any) {
 
     return (
         <div>
-            <Flex justify="space-between" align="center" mb={2}>
-                <Avatar size="lg" src={avatarUrl} />
-                <Text fontSize="lg" fontWeight="bold" textAlign="center">Receive</Text>
-                <br/>
-                {selectedAddress}
-                {app.assetContext.pubkeys[0].pubkey}
-                {/*<QRCode value={selectedAddress || ""} size={64} />*/}
-            </Flex>
+          <Card.Root maxW="sm" overflow="hidden">
+            <Avatar size="xl" src={avatarUrl} />
+            <Card.Body gap="2">
+              <Card.Title>Receive</Card.Title>
+              <Card.Description>
 
-            <Flex align="center" justify="center" mt={2}>
-                {/*<Button size="sm" onClick={onCopy}>{hasCopied ? 'Copied' : 'Copy Address'}</Button>*/}
-            </Flex>
+              </Card.Description>
+              <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
+                <br/>
+                <br/>
+                {app.assetContext.pubkeys[0].master || app.assetContext.pubkeys[0].pubkey}
+              </Text>
+            </Card.Body>
+            <Card.Footer gap="2">
+              {/*<Button variant="solid">Buy now</Button>*/}
+              {/*<Button variant="ghost">Add to cart</Button>*/}
+            </Card.Footer>
+          </Card.Root>
         </div>
     );
 }
