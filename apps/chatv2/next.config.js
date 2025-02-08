6488d03fe3c0d648cfcf16b10e959e6649ea486c
highlander <bithighlander@gmail.com>
2025-02-08 13:51:15 -0600

    debug build

--- apps/chatv2/next.config.js
+++ apps/chatv2/next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  reactStrictMode: true,
+  transpilePackages: ['@coinmasters/pioneer-react'],
 }
 
 module.exports = nextConfig 
