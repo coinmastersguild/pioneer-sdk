5eac16fbf78b43158d25fd30e72299c424d8f5d4
highlander <bithighlander@gmail.com>
2025-02-10 17:35:19 -0600

    clean up

--- apps/chat/src/components/chat/constants.ts
+++
@@ -1,9 +0,0 @@
-import { HiHeart } from "react-icons/hi";
-import { AvatarMap } from './types';
-
-export const AVATARS: AvatarMap = {
-  user: HiHeart,
-  computer: 'https://pioneers.dev/coins/keepkey.png'
-};
-
-export const TAG = " | Chat | "; 
