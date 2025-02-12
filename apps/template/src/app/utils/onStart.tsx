6fde14a4bb59b75b96838824b898cca11b320d56
highlander <bithighlander@gmail.com>
2025-02-12 13:14:58 -0600

    begin template

---
+++ apps/template/src/app/utils/onStart.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { toaster } from '@/components/ui/toaster';
+import { usePioneer } from '@coinmasters/pioneer-react';
+
+export const useOnStartApp = () => {
+    const { onStart } = usePioneer();
+y
+    const onStartApp = async () => {
+        try {
+            const pioneerSetup = {
+                appName: 'KeepKey Portfolio',
+                appIcon: 'https://pioneers.dev/coins/keepkey.png',
+            };
+
+            const events = await onStart([], pioneerSetup);
+
+            // Subscribe to all events
+            events.on('*', (action: string, data: any) => {
+                console.log('Event: ',action, data);
+                toaster.create({
+                    title: `Event: ${action}`,
+                    description: JSON.stringify(data),
+                    duration: 5000,
+                });
+            });
+        } catch (e) {
+            console.error('Failed to start app!', e);
+        }
+    };
+
+    return onStartApp;
+};
